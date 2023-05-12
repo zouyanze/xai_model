eb5bc3ccb96e6ba9b562dacf9f722423d4f57778
Zou Yanze <91880399+zouyanze@users.noreply.github.com>
2023-05-12 12:22:14 +0900

    Update data_clean.py

--- data_clean.py
+++ data_clean.py
@@ -7,7 +7,7 @@
 import pandas as pd
 rankings_colname=['label','title','text']
 agnews_train = pd.read_csv('train.csv',encoding='utf-8',engine='python',header=None,names=rankings_colname)
-agnews_train  ##结果如下图
+agnews_train  
 
 
 # In[16]:
@@ -16,9 +16,9 @@ agnews_train  ##结果如下图
 import re
 def text_clear(text):
     text = text.lower()
-    text = re.sub(r'[^a-z0-9]',' ',text)    #用空格替换非标准字符，^为求反操作
-    text = re.sub(r' +',' ',text)   # 替换多重空格为单空格
-    text = text.strip()     #去除首尾空格
+    text = re.sub(r'[^a-z0-9]',' ',text)    #clean
+    text = re.sub(r' +',' ',text)   # replace space
+    text = text.strip()     #delete space
     return text
 
 
@@ -30,10 +30,10 @@ import numpy as np
 agnews_label = []
 agnews_title = []
 agnews_text = []
-## 便于处理数据
+
 agnews_train = csv.reader(open('train.csv','r'))
 for line in agnews_train:
-     ## 对于数字，得数字化转换；对于文字，得格式处理
+     ## for number and text
      #print(line)
      agnews_label.append(np.float32(line[0]))
      agnews_title.append(line[1])
@@ -43,7 +43,7 @@ merge = pd.DataFrame({
     'title':agnews_title,
     'text':agnews_text
 })
-merge  ##可观察到处理后的数据与原始数据的不同
+merge  
 
 
 # In[18]:
