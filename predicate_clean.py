c4d9da734ab8fcf0919d6e710d39aa4285dab9e3
Zou Yanze <91880399+zouyanze@users.noreply.github.com>
2023-05-12 12:23:40 +0900

    Update predicate_clean.py

--- predicate_clean.py
+++ predicate_clean.py
@@ -7,13 +7,13 @@
 import csv
 import re
 
-input_file = 'predicates_test.csv'  # 输入表格文件名
-output_file = 'predicate.csv'  # 输出表格文件名
+input_file = 'predicates_test.csv'  
+output_file = 'predicate.csv'  
 
-# 定义正则表达式模式，用于匹配数字和点号
+
 pattern = r'\d+|\.'
 
-# 读取输入表格文件并处理数据
+
 with open(input_file, 'r') as f_in, open(output_file, 'w', newline='') as f_out:
     reader = csv.reader(f_in)
     writer = csv.writer(f_out)
@@ -21,13 +21,12 @@ with open(input_file, 'r') as f_in, open(output_file, 'w', newline='') as f_out:
     for row in reader:
         processed_row = []
         for cell in row:
-            # 去除数字和点号
+            # delete number and .
             processed_cell = re.sub(pattern, '', cell)
             processed_row.append(processed_cell)
 
         writer.writerow(processed_row)
 
-print("处理完成，结果保存到output.csv文件中。")
 
 
 # In[ ]:
